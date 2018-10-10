48f46410cb383cc072c4248f93dd7444d9d6eb8b
Christian Alexsander da Silva Santos <christianalexbh@hotmail.com>
2018-10-09 23:00:58 -0300

    Feed Card model - Post Create page - Theme Color Variabel

--- marcate/src/app/app.module.ts
+++ marcate/src/app/app.module.ts
@@ -9,9 +9,11 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-import { FeedPage } from '../pages/feed/feed'
-import { PostCreatPage } from '../pages/feed/post-creat/post-creat'
+import { FeedPage } from '../pages/feed/feed';
+import {PostPage} from '../pages/feed/post/post'
+
 import { PostViewPage } from  '../pages/feed/post-view/post-view'
+import {HeaderPage} from '../pages/header/header'
 import { FIREBASE_CONFIG } from './app.firebase.config';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -25,8 +27,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     TabsPage,
     FeedPage,
-    PostCreatPage,
-    PostViewPage
+    PostViewPage,
+    HeaderPage,
+    PostPage
   ],
   imports: [
     BrowserModule,
@@ -42,8 +45,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     TabsPage,
     FeedPage,
-    PostCreatPage,
-    PostViewPage
+    PostViewPage,
+    HeaderPage,
+    PostPage
   ],
   providers: [
     StatusBar,
